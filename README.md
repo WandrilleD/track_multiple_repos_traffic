c97ff8ebb55105478bf08924af0c44682ecd7d50
WandrilleD <wandrille.duchemin@unibas.ch>
2025-10-31 16:51:36 +0100

    initial test: sangonzal action + my script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # track_multiple_repos_traffic
-demo of some code to track the traffic on multiple repos beyond the 15 days limit
+
+Demo of some code to track the traffic on multiple repos beyond the 15 days limit
+
+
+> this repo draws a lot of inspiration from the [repository-traffic-action](https://github.com/sangonzal/repository-traffic-action). We encourage you to have a look at it if this sort of things are interesting to you.
+
+
+Using github actions, we execute a python script (`scan_multiple_github_repo_traffic.py`) which looks at the github repositories listed in `repos_to_watch.txt`.
+
+The traffic information is stored and updated in the `traffic/` folder.
+
+
+
+To be able to access the traffic information, we have defined a "TRAFFIC_ACTION_TOKEN" secret on this repo which contains a Personal Access Token with the requisite accesses to the tracked repositories (for a fine-grained PAT, you need to allow permission for "administration", read-only is enough).
+
---
+++ scan_multiple_github_repo_traffic.py
@@ -0,0 +1,170 @@
+import os
+import sys
+from datetime import timedelta, datetime
+
+from github import Github, Auth
+
+def write_table( data , repo_list , file = sys.stdout ):
+    """writes view/clone data as a csv file
+    data is expected to be a dictionary
+    whose keys are datetime
+    and values are dictionaries
+        whose keys are repo name
+        and values are corresponding value (eg, number of views for a given date for a given repo)
+    """
+
+    print( "date" , *repo_list , sep = ',' , file=file )
+    for d in data:
+        print( d , *[data[d][r] for r in repo_list ] , sep = ',' , file=file )
+    
+def read_table( file ):
+    """
+    reads a csv file and returns it as a dictionary
+    whose keys are datetime
+    and values are dictionaries
+        whose keys are repo name
+        and values are corresponding value (eg, number of views for a given date for a given repo)
+    """
+    header = file.readline().strip()
+    repo_list = header.split(',')[1:]
+    
+    data = {}
+    
+    for l in file:
+        sl = l.strip().split(',')
+        date = datetime.fromisoformat( sl[0] )
+        data[ date ] = {}
+        
+        for i,count in enumerate( sl[1:] ):
+            data[ date ][ repo_list[i] ] = count
+    
+    return data
+
+def get_metrics( repo , github_object ):
+    """ given 1 repository name and a github object, gather views and clones data.
+
+    returns the data in a dictionary whose keys are 'view_count','view_unique','clone_count', or 'clone_unique'
+    and whose values are dictionaries whose keys are datetime and value are the corresponding value (ie, number of views, or clone,..)
+    """
+    repo = github_object.get_repo( repo )
+    
+    data = {}
+    data['view_count'] = {}
+    data['view_unique'] = {}
+    data['clone_count'] = {}
+    data['clone_unique'] = {}
+
+    
+    views = repo.get_views_traffic()
+    for v in views.views:
+        data['view_count'][v.timestamp]  = v.count
+        data['view_unique'][v.timestamp] = v.uniques
+
+    clones = repo.get_clones_traffic()
+    for c in clones.clones:
+        data['clone_count'][c.timestamp]  = c.count
+        data['clone_unique'][c.timestamp] = c.uniques
+        
+    return data
+
+def complement_data_structure( min_date,  max_date, repo_list, data = {} ):
+    """creates or complement data structure to store 1 metric
+    data is expected to be a dictionary
+    whose keys are datetime
+    and values are dictionaries
+        whose keys are repo name
+        and values are corresponding value (eg, number of views for a given date for a given repo)
+    """
+    
+    if len(data)>0: ## there is already data 
+        # -> update minimum date
+        min_date = min(min_date , *(data.keys()) )
+        # -> update maximum date
+        max_date = max(max_date , *(data.keys()) )
+        
+        # -> add repos data if they are absent. initialize their value to NA
+        for k in data:
+            for r in repo_list:
+                if not r in data[k]:
+                    data[k][r] = "NA"
+    
+    ## making sure we have data for all days. initializing all at 0
+    delta = timedelta(days=1)
+    current = min_date
+    while current <= max_date:
+        if not current in data: # date absent from the current data -> initialize all at 0
+            data[current] = { r:0 for r in repo_list }
+        current += delta
+        
+    return data
+
+
+# using an access token
+auth = Auth.Token(os.environ["TRAFFIC_ACTION_TOKEN"])
+
+# Public Web Github
+g = Github(auth=auth)
+
+
+repo_list_file = sys.argv[1]
+
+
+## files for view_count, view_unique, clone_count, clone_unique
+files = { 'view_count' : sys.argv[2],
+          'view_unique' : sys.argv[3],
+          'clone_count' : sys.argv[4],
+          'clone_unique' : sys.argv[5]}
+
+## reading the repo list
+repo_list = []
+with open(repo_list_file) as IN:
+    for l in IN:
+        repo_list.append( l.strip() )
+
+
+
+## reading the data we already have
+pre_data = {}
+for k in files:
+    pre_data[k] = {}
+    if os.path.exists(files[k]):
+        with open(files[k]) as IN:
+            pre_data[k] = read_table(IN)
+
+
+## gathering the data from github
+raw_data = {}
+for r in repo_list:
+    raw_data[r] = get_metrics( repo = r, 
+                               github_object = g )
+
+
+## determining the time window of the data
+all_dates = set()
+for r,data in raw_data.items():
+    for data2 in data.values():
+        all_dates.update( data2.keys() )
+min_date , max_date = min(all_dates) , max(all_dates)
+
+## adding the new dates to the data structure
+data = {}
+
+for k in pre_data:
+    data[k] = complement_data_structure( min_date,  max_date, 
+                                        repo_list=repo_list, 
+                                        data = pre_data[k] )
+
+
+## adding the new data 
+for k in pre_data:
+    for repo in raw_data:
+        
+        for date,value in raw_data[repo][k].items():
+            data[k][date][repo] = value
+
+
+## writing data to files
+for k in data:
+    with open( files[k],'w') as OUT:
+        write_table( data[k] , repo_list , file = OUT )
+
